5126a2eea274e7f89092972a7572b76bbf4c4dcd
juancenteno10 <juancsabillon06@gmail.com>
2023-05-28 21:50:19 -0600

    pa merge

--- Plantilla/NobleUI Pruebas (Version Actualizada)/demo1/src/app/Models/roles.ts
+++
@@ -1,8 +0,0 @@
-export class roles{
-    role_ID!: string;
-    role_Descripcion!: string;
-    usua_Usuario!: number;
-    role_FechaCreacion!: Date;
-    role_UsuarioModificador!: number;
-    role_FechaModificacion: Date;
-}
